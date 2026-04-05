74112d5552ce4a33b1a876c94a82e9d41cb90305
tlsloth <laizw02@hotmail.com>
2026-04-05 20:17:17 +0800

    fix: extra param in ble

--- pico_mpr_bridge/main.py
+++ pico_mpr_bridge/main.py
@@ -455,7 +455,7 @@ def main():
         # BLE Tasks
         if ble_ok:
             tasks.append(asyncio.create_task(
-                ble_interface.rx_task(ingress_queue, ble_egress, neighbour_table)))  # ← must be here
+                ble_interface.rx_server_task(ingress_queue, neighbour_table)))
             tasks.append(asyncio.create_task(
                 ble_interface.tx_task(ble_egress, neighbour_table)))
             tasks.append(asyncio.create_task(
